8b0cd5333a641782eb2beafb3ce42820f73ba0c6
VoDinhNghia <vodinhnghia85@gmail.com>
2023-08-24 08:10:51 +0700

    add event onClick button

--- src/gui/GuiMain.java
+++ src/gui/GuiMain.java
@@ -3,15 +3,18 @@ package gui;
 import java.awt.*;
 import java.awt.event.*;
 
+import gui.actions.CaculatorAction;
+
 public class GuiMain extends Frame {
     public GuiMain() {
-        Button btn = new Button("Test");
+        Button btn = new Button("Calculator");
         btn.setBounds(50, 50, 50, 50);
         add(btn);
-        setSize(150, 150);
-        setTitle("This is my First AWT example");
+        setSize(400, 500);
+        setTitle("Caculator program");
         setLayout(new FlowLayout());
         setVisible(true);
+        btn.addActionListener(new CaculatorAction());
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent we) {
                 dispose();
