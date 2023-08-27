68bd5dca2c0a981abad168c4705901570d544430
VoDinhNghia <vodinhnghia85@gmail.com>
2023-08-27 19:39:12 +0700

    login frame

--- src/gui/GuiMain.java
+++ src/gui/GuiMain.java
@@ -1,25 +1,9 @@
 package gui;
 
-import java.awt.*;
-import java.awt.event.*;
+import gui.screens.LoginGUI;
 
-import constants.TextGui;
-import gui.actions.CalculatorAction;
-
-public class GuiMain extends Frame {
+public class GuiMain {
     public GuiMain() {
-        Button btn = new Button(TextGui.btnCalculator);
-        btn.setBounds(50, 50, 50, 50);
-        add(btn);
-        setSize(400, 500);
-        setTitle(TextGui.titleMain);
-        setLayout(new FlowLayout());
-        setVisible(true);
-        btn.addActionListener(new CalculatorAction());
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent we) {
-                dispose();
-            }
-        });
+       new LoginGUI();
     }
 }
