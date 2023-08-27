63d88aadec592d4a27e1a435f6c85889349e2f65
VoDinhNghia <vodinhnghia85@gmail.com>
2023-08-27 16:06:00 +0700

    generate user code and crypto password

--- src/configs/ConnectDb.java
+++ src/configs/ConnectDb.java
@@ -1,4 +1,4 @@
-package database;
+package configs;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
---
+++ src/constants/TableName.java
@@ -0,0 +1,5 @@
+package constants;
+
+public class TableName {
+    public static final String users = "users";
+}
